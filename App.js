3c6779cc11b55111f23a1d0012b611cad7439989
Amjad Khan <amkh@microsoft.com>
2018-09-22 12:11:35 +0530

    Added collection Quotes

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Quote } from "./Component/Quote";
+import { Quotes } from "./Component/Quotes";
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>App is testing from the VS code</Text>
-        <Quote
-          QuoteText="Always think BIG, as thoughts does not have any limits!"
-          QuoteAuthor="Amjad Khan W."
-        />
+        <Quotes />
       </View>
     );
   }
@@ -17,7 +14,7 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 0.3,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
